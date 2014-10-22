ec6a06ad0c26005f460c63478b058f8a78b2a176
userbz <bo.zhang@ki.se>
2014-10-23 01:11:06 +0200

    Fix 0_Exe_pipline.py
    fixed a coding error

--- 0_Exe_pipeline.py
+++ 0_Exe_pipeline.py
@@ -5,6 +5,7 @@ apars.add_argument('-exe', default=r'"ExecutePipeline.exe"')
 apars.add_argument('-topp', default=r'TOPP_Processing.toppas')
 apars.add_argument('-out_dir', default='.')
 apars.add_argument('-trf', default=tempfile.mktemp('.trf'))
+args = apars.parse_args()
 
 trf = '''<?xml version="1.0" encoding="ISO-8859-1"?>
 <PARAMETERS version="1.6.2" xsi:noNamespaceSchemaLocation="http://open-ms.sourceforge.net/schemas/Param_1_6_2.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -15,9 +16,8 @@ trf = '''<?xml version="1.0" encoding="ISO-8859-1"?>
   </NODE>
 </PARAMETERS>
 ''' % urllib.pathname2url(args.mzml)
+
+
 open(args.trf, 'w').write(trf)
-
-
-args = apars.parse_args()
 cmd =  args.exe + ' -in ' + args.topp + ' -resource_file ' + args.trf + ' -out_dir ' + args.out_dir 
 subprocess.call(cmd)
