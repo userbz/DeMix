63041beaf3b132fa6f1e7a0c2cc58780b6c97519
userbz <bo.zhang@ki.se>
2014-11-19 13:49:36 +0100

    Add Example
    Added one example spectrum file;
    Fixed some errors when calling system commands;
    Tested cross platform compatibility

--- ver01/README.md
+++ ver01/README.md
@@ -21,6 +21,23 @@ MS-GF+ http://omics.pnl.gov/software/ms-gf
 
 
 
+##### Example:
+
+* Windows Command Prompt   
+python C:\GitHub\DeMix\ver01\run_demix_pipeline.py -exe "c:\Program Files\OpenMS-1.11\bin\ExecutePipeline.exe" -db c:\DATA\HUMAN.fa -w 4.0 C:\GitHub\DeMix\Example\20131106_Q2_SDC_120MIN_HELA1.mzML.gz
+
+* Linux or OSX Terminal   
+python /home/GitHub/DeMix/ver01/run_demix_pipeline.py /home/GitHub/DeMix/Example/20131106_Q2_SDC_120MIN_HELA1.mzML.gz -db /Data/HUMAN.fa -w 4.0 -out_dir DeMixResult
+
+Parameters: 
+*   -db : Fasta file of proteome sequences (might require writing permit on the path for MS-GF+ indexing) 
+*   -w : full width of precursor isolation window, default 4.0  
+*   -out_dir : root path for outputs, default: current working path ".";  * Spectra and identification result will be put in: out_dir/TOPPAS_out/004-PeakPickerHiRes/
+* Set these options only for debug purpose:   
+-exe : when "ExecutePipeline" is not found in system environment PATH.  
+-topp : when the default TOPPAS workflow is not working.   
+-trf : when failed generating temporary resource file. 
+
 ##### Reference
 Zhang, B., Pirmoradian, M., Chernobrovkin, A., & Zubarev, R. a. (2014). DeMix Workflow for Efficient Identification of Co-fragmented Peptides in High Resolution Data-dependent Tandem Mass Spectrometry. Molecular & Cellular Proteomics : MCP. doi:10.1074/mcp.O114.038877      
 http://www.ncbi.nlm.nih.gov/pubmed/25100859
--- ver01/run_demix_pipeline.py
+++ ver01/run_demix_pipeline.py
@@ -8,18 +8,27 @@ import tempfile
 import argparse
 import os
 
-
 apars = argparse.ArgumentParser()
+
+# Set ONE mzML file converted from RAW file, require both MS1 and MS2 in profile mode. 
 apars.add_argument('mzml')
-apars.add_argument('-exe', default='ExecutePipeline.exe')
-apars.add_argument('-topp', default=os.path.join(os.path.dirname(sys.argv[0]), 'TOPP_Processing.toppas'))
+# Set path to the reference proteome FASTA file. (require writing permit of the same path)
 apars.add_argument('-db', default='HUMAN.fa')
+# Set a out put path. (requre writing permit of the path)
 apars.add_argument('-out_dir', default='.')
-apars.add_argument('-trf', default=tempfile.mktemp('.trf'))
+# Set the full width of precursor isolation window
 apars.add_argument('-w', default=4.0)
 
-args = apars.parse_args()
 
+# When not found in system PATH, specify the absolute path of ExecutePipeline.exe. 
+apars.add_argument('-exe', default='ExecutePipeline')
+# When using user customized TOPP workflow
+apars.add_argument('-topp', default=os.path.join(os.path.dirname(sys.argv[0]), 'TOPP_Processing.toppas'))
+# When loading a user coustomized TOPP resource file. 
+apars.add_argument('-trf', default=tempfile.mktemp('.trf'))
+
+#=======
+args = apars.parse_args()
 trf = '''<?xml version="1.0" encoding="ISO-8859-1"?>
 <PARAMETERS version="1.6.2" xsi:noNamespaceSchemaLocation="http://open-ms.sourceforge.net/schemas/Param_1_6_2.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <NODE name="1" description="">
@@ -32,35 +41,33 @@ trf = '''<?xml version="1.0" encoding="ISO-8859-1"?>
 open(args.trf, 'w').write(trf)
 
 # '======= run TOPP workflow for peak picking and feature detection'
-cmd = args.exe  + ' -in ' + args.topp \
-				+ ' -resource_file ' + args.trf \
-				+ ' -out_dir ' + args.out_dir
-print cmd
+cmd = [	args.exe, 
+		'-in', args.topp,
+	 	'-resource_file', args.trf, 
+		'-out_dir', args.out_dir ]
+print ' '.join(cmd)
 subprocess.call(cmd)
-
 
 # '======= first pass MS-GF+ database searching '
 centroidSpec = os.path.join(args.out_dir,  'TOPPAS_out', '004-PeakPickerHiRes', os.path.basename(args.mzml).replace('.gz', ''))
-cmd = 'java -Xmx8g -jar ' + os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'MSGFPlus.jar')
-
-# change PTMs in the Mods.txt file under MSGFPlus folder.
-cmd += ' -mod ' + os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'Mods.txt') \
- 	+ ' -d ' + args.db \
-	+ ' -s ' + centroidSpec \
-	+ ' -o ' + centroidSpec + '.mzid'
-
+cmd = [ 'java', '-Xmx8g',
+		'-jar', os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'MSGFPlus.jar'),
+		'-mod', os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'Mods.txt'),
+		'-d', args.db,
+		'-s', centroidSpec,
+		'-o', centroidSpec + '.mzid' ] 
 # Q-Exactive, HCD, trypsin, 2 miscleavage, 0/1 13C, 10 ppm precursor tol.
-cmd += ' -t 10ppm -ti 0,1 -m 3 -inst 3 -minLength 7 -addFeatures 1 -tda 1 '
-print cmd
+cmd += '-t 10ppm -ti 0,1 -m 3 -inst 3 -minLength 7 -addFeatures 1 -tda 1'.split()
+print ' '.join(cmd)
 subprocess.call(cmd)
 
 
 # '======== convert mzid to tsv '
-cmd = 'java -Xmx8g -cp ' \
-		+ os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'MSGFPlus.jar') \
-		+ ' edu.ucsd.msjava.ui.MzIDToTsv -i ' \
-		+ centroidSpec + '.mzid'
-print cmd
+cmd = ['java', '-Xmx8g',
+		'-cp', os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'MSGFPlus.jar'),
+		'edu.ucsd.msjava.ui.MzIDToTsv', 
+		'-i', centroidSpec + '.mzid' ]
+print ' '.join(cmd)
 subprocess.call(cmd)
 
 
@@ -70,6 +77,8 @@ psm = feature_ms2_clone.load_PSM(centroidSpec + '.tsv')
 t = psm['PrecursorError(ppm)'].std() * 3
 t = t > 10 and 10 or t
 
+print psm.describe()
+demixSpec = centroidSpec + '.demix.mgf'
 featureTab = os.path.join(args.out_dir,  'TOPPAS_out', '009-TextExporter',
                           os.path.basename(args.mzml).replace('.gz', '').replace('.mzML', '.csv'))
 demixSpec = feature_ms2_clone.spectra_clone(
@@ -77,13 +86,16 @@ demixSpec = feature_ms2_clone.spectra_clone(
 
 
 # '======== second pass MS-GF+ dababase search '
-cmd = 'java -Xmx8g -jar ' + os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'MSGFPlus.jar') \
-	+ ' -mod ' + os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'Mods.txt') \
-	+ ' -d ' + args.db \
-	+ ' -s ' + demixSpec \
-	+ ' -o ' + demixSpec + '.mzid'
-
-cmd += ' -ti 0,1 -m 3 -inst 3 -minLength 7 -maxLength 50 -addFeatures 1 -tda 1 '
-cmd += ' -t %.1fppm ' % t
-print cmd
+cmd = [ 'java', '-Xmx8g',
+		'-jar', os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'MSGFPlus.jar'),
+		'-mod', os.path.join(os.path.dirname(args.topp), 'MSGFPlus', 'Mods.txt'),
+		'-d', args.db,
+		'-s', demixSpec,
+		'-o', demixSpec + '.mzid',
+		'-t', '%.1fppm' % t] # adaptive mass tolerance
+cmd += '-ti 0,1 -m 3 -inst 3 -minLength 7 -addFeatures 1 -tda 1'.split()
+print ' '.join(cmd)
 subprocess.call(cmd)
+
+
+
